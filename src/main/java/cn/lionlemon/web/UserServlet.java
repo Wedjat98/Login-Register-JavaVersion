406522a7a71f89bf9485669df7da38241e41605c
lionlemon <ame9807@gmail.com>
2021-06-15 16:29:27 +0900

    第2次测试 Spring集成web环境

--- src/main/java/cn/lionlemon/web/UserServlet.java
+++ src/main/java/cn/lionlemon/web/UserServlet.java
@@ -1,29 +1,27 @@
 package cn.lionlemon.web;
-
-import cn.lionlemon.config.SpringConfiguration;
 import cn.lionlemon.service.UserService;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import java.io.IOException;
 
 @WebServlet("/user")
 public class UserServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserService userService = app.getBean(UserService.class);
-        userService.save();
-    }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.doGet(req,resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ServletContext servletContext = this.getServletContext();
+        WebApplicationContext app = WebApplicationContextUtils.getWebApplicationContext((javax.servlet.ServletContext) servletContext);
+        UserService userService =  app.getBean(UserService.class);
+        userService.save();
+        System.out.println(servletContext);
     }
 }
